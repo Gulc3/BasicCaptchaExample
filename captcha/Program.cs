cd812fd3dd0f75abec65525153be05f3de06c0fa
Gülce Gül <86388554+Gulc3@users.noreply.github.com>
2022-08-26 00:31:19 +0300

    Add files via upload

---
+++ captcha/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d1, d2, d3, d4;
+            Random rn = new Random();
+
+            d1 = rn.Next(0,10);
+            d2 = rn.Next(0, 10);
+            d3 = rn.Next(0, 10);
+            d4 = rn.Next(0, 10);
+
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            string[] karakterler = { "a", "A", "b ", "B", "c", "C", "d", "D", "e", "E" };
+            Console.Write(d1 + karakterler[d2] + d3 + karakterler[d4] );
+
+
+            Console.Read();
+
+        }
+    }
+}
